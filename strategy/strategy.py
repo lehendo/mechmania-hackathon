4b8fdee7f7b306ced0ab5381830617193716f925
Arjun Chatterjee <arj0jeechat@gmail.com>
2024-09-22 15:35:55 -0500

    Update strategy.py

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -1,6 +1,8 @@
 import random
 from game.base_strategy import BaseStrategy
-from game.plane import Plane, PlaneType
+from game.plane import Plane, PlaneType, Vector
+import math
+import strategy.utils as utils
 
 # The following is the heart of your bot. This controls what your bot does.
 # Feel free to change the behavior to your heart's content.
@@ -12,109 +14,64 @@ class Strategy(BaseStrategy):
     # You can define whatever variables you want here
     my_counter = 0
     my_steers = dict()
+
     
     def select_planes(self) -> dict[PlaneType, int]:
         # Select which planes you want, and what number
         return {
-            PlaneType.STANDARD: 1,
+            PlaneType.STANDARD: 0,
             PlaneType.FLYING_FORTRESS: 1,
-            PlaneType.THUNDERBIRD: 1,
-            PlaneType.SCRAPYARD_RESCUE: 1,
+            PlaneType.THUNDERBIRD: 3,
+            PlaneType.SCRAPYARD_RESCUE: 0,
+            PlaneType.PIGEON: 10
         }
     
     def steer_input(self, planes: dict[str, Plane]) -> dict[str, float]:
-        # Define a dictionary to hold our response
         response = dict()
+        enemy_planes = []
+        for id, plane in planes.items():
+            if (plane.team != self.team):
+                enemy_planes.append(plane)
+        # Define a dictionary to hold our response
+        #nearest plane to current plane
 
+        response = dict()  
         # For each plane
         for id, plane in planes.items():
             # id is the unique id of the plane, plane is a Plane object
-
             # We can only control our own planes
             if plane.team != self.team:
                 # Ignore any planes that aren't our own - continue
                 continue
-            self.my_steers[id] = 0
+            nearest_plane = plane
+            min2 = 100000
+            for plane2 in enemy_planes:
+                if (math.sqrt((plane2.position.x - plane.position.x)**2 + (plane2.position.y - plane.position.y)**2) < min2):
+                    min2 = math.sqrt((plane2.position.x - plane.position.x)**2 + (plane2.position.y - plane.position.y)**2)
+                    nearest_plane = plane2
             # If we're within the first 5 turns, just set the steer to 0
-            if(plane.position.x > 35):
-                if(plane.angle > 0 and plane.angle < 90):
-                    self.my_steers += 0.5
-                elif(plane.angle > 270):
-                    self.my_steers = -0.5
-            if(plane.position.x < -35):
-                if(plane.angle > 90 and plane.angle < 180):
-                    self.my_steers += 0.5
-                if(plane.angle > 180 and plane.angle < 270):
-                    self.my_steers -= 0.5
-
-            if(plane.position.y > 35):
-                if(plane.angle > 90 and plane.angle < 180):
-                    self.my_steers[id] -= 0.5
-                if(plane.angle > 0 or plane.angle < 90):
-                    self.my_steers[id] += 0.5
+            #create tuple for plane direction vector
+            '''
+            vector_1 = Vector(math.cos(plane.angle), math.sin(plane.angle)) 
+            x_diff = nearest_plane.position.x - plane.position.x
+            y_diff = nearest_plane.position.y - plane.position.y
+            vector_2 = Vector(x_diff, y_diff)  
+            #gives us the angle between our plane and its current target
+            target_angle = utils.angle_between_vectors(vector_1, vector_2)
+            if x_diff < 0:
+                self.my_steers[id] = -1 * target_angle/
+            '''
+            vector = Vector(nearest_plane.position.x, nearest_plane.position.y)
             
-            if(self.my_steers[id] > 1):
-                self.my_steers[id] = 1
-            if(self.my_steers[id] < -1):
-                self.my_steers[id] = -1
-            response[id] = self.my_steers[id]
+            x_diff = nearest_plane.position.x - plane.position.x
+            a = utils.plane_find_path_to_point(vector,plane)
+            response[id] = a[1] + a[0]
+            if(response[id] < -1) :
+                response[id] = -1
+            if(response[id] > 1):
+                response[id] = 1
         # Increment counter to keep track of what turn we're on
         self.my_counter += 1
 
         # Return the steers
         return response
-import random
-from game.base_strategy import BaseStrategy
-from game.plane import Plane, PlaneType
-
-# The following is the heart of your bot. This controls what your bot does.
-# Feel free to change the behavior to your heart's content.
-# You can also add other files under the strategy/ folder and import them
-
-class Strategy(BaseStrategy):
-    # BaseStrategy provides self.team, so you use self.team to see what team you are on
-
-    # You can define whatever variables you want here
-    my_counter = 0
-    my_steers = dict()
-    
-    def select_planes(self) -> dict[PlaneType, int]:
-        # Select which planes you want, and what number
-        return {
-            PlaneType.STANDARD: 1,
-            PlaneType.FLYING_FORTRESS: 1,
-            PlaneType.THUNDERBIRD: 1,
-            PlaneType.SCRAPYARD_RESCUE: 1,
-            PlaneType.PIGEON: 10,
-        }
-    
-    def steer_input(self, planes: dict[str, Plane]) -> dict[str, float]:
-        # Define a dictionary to hold our response
-        response = dict()
-
-        # For each plane
-        for id, plane in planes.items():
-            # id is the unique id of the plane, plane is a Plane object
-
-            # We can only control our own planes
-            if plane.team != self.team:
-                # Ignore any planes that aren't our own - continue
-                continue
-
-            # If we're within the first 5 turns, just set the steer to 0
-            if self.my_counter < 5:
-                response[id] = 0
-            else:
-                # If we haven't initialized steers yet, generate a random one for this plane
-                if id not in self.my_steers:
-                    self.my_steers[id] = random.random() * 2 - 1
-
-                # Set the steer for this plane to our previously decided steer
-                response[id] = self.my_steers[id]
-
-        # Increment counter to keep track of what turn we're on
-        self.my_counter += 1
-
-        # Return the steers
-        return response
-
